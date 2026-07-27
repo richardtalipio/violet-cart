d132e56fd3bd7f8996c950865d2d5a45f8af3d9a
Richard Alipio <richardtalipio@gmail.com>
2026-07-27 16:51:36 +0800

    Start VioletCart Backend project setup

---
+++ violetCart-backend/src/test/java/com/violetCart/violetCart_backend/VioletCartBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.violetCart.violetCart_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VioletCartBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
